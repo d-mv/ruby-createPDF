267f6c434e71e22806156f7986626ea4e6088a60
d-mv <d.dash.mv@gmail.com>
2019-01-06 13:53:18 +0200

    Added link to tutorial

--- lib/start.rb
+++ lib/start.rb
@@ -1,4 +1,6 @@
 # https://github.com/pdfkit/pdfkit
+# https://code.tutsplus.com/tutorials/generating-pdfs-from-html-with-rails--cms-22918
+
 require 'pdfkit'
 
 PDFKit.new('https://en.wikipedia.org/wiki/Google').to_file('google.pdf')
